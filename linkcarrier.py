11f90c46c7719f50a222476a833f6bbb5f6fc19c
J1NY0UNG <jinyoung3635@gmail.com>
2022-08-22 16:48:15 +0900

    Operation possible

--- linkcarrier.py
+++ linkcarrier.py
@@ -1,12 +1,13 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-html = urlopen("https://news.naver.com/")
+html = urlopen("https://www.youtube.com/c/Owlsreview/videos")
 
 bsObject = BeautifulSoup(html, "html.parser")
 
-f = open("C:\\Users\\User\\Desktop\\ddolddol\\project\\dashspace\\cinema-reviewer\\linklist.txt" , 'w')
+f = open("C:\\Users\\User\\Desktop\\ddolddol\\Review-Filmer\\linklist.txt" , 'w')
 
 for link in bsObject.find_all('a'):
-    print(link.get('href'))
+    f.write(link.get('href'))
 
+f.close
