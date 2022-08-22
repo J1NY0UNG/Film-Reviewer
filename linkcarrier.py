2fb7d3c9765395b7aa1e3add6a69081495620e3e
J1NY0UNG <jinyoung3635@gmail.com>
2022-08-22 22:10:10 +0900

    타이틀& 링크 가져오기 가능, 쓰기 오류 있음

--- linkcarrier.py
+++ linkcarrier.py
@@ -1,13 +1,21 @@
-from urllib.request import urlopen
+from selenium import webdriver
 from bs4 import BeautifulSoup
 
-html = urlopen("https://www.youtube.com/c/Owlsreview/videos")
+driver =  webdriver.Chrome()
+driver.implicitly_wait(3)
 
-bsObject = BeautifulSoup(html, "html.parser")
+driver.get('https://www.youtube.com/c/Owlsreview/videos')
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+titles = soup.select('#video-title')
+links = soup.find_all('a', id='video-title')
 
-f = open("C:\\Users\\User\\Desktop\\ddolddol\\Review-Filmer\\linklist.txt" , 'w')
+f = open("C:\\Users\\User\\Desktop\\ddolddol\\Review-Filmer\\linklist.txt", "w", encoding='utf-8')
 
-for link in bsObject.find_all('a'):
-    f.write(link.get('href'))
+for n in titles:
+    f.write(n.text.strip())
+
+for l in links:
+    f.write('youtube.com' + l.get('href'))
 
 f.close
